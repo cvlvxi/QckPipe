e2dc8ed1a0baadd484917938002ead10162a759f
cvlvxi <cvlvxi@outlook.com>
2021-04-27 18:23:55 +1000

    Add MonadFail and Alternative implementation for Parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,56 @@
 module Main where
 
-
 newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 
-
-
-
 instance Functor Parser where 
-    fmap fn Parser {runParser = runFn } = Parser { runParser = \inputStr -> do
-            (output, remainderStr) <- runFn  inputStr
+    fmap fn Parser {runParser = runFn } = Parser { runParser = \s -> do
+            (output, rest) <- runFn s
             -- Call the function on theoutput 
-            return (fn output, remainderStr)
+            return (fn output, rest)
         }
 
 instance Applicative Parser where
-    pure x = Parser {runParser = \inputStr -> Just(x, inputStr)}
+    pure x = Parser {runParser = \s -> Just(x, s)}
 
-    Parser {runParser = runFn1 } <*> Parser {runParser = runFn2 } = Parser { runParser = \inputStr -> do
-            (outputFn, remainder1) <- runFn1  inputStr
-            (parserOutput, remainderStr2) <- runFn2 remainder1 
-            return (outputFn parserOutput, remainderStr2)
+    Parser {runParser = runFn1 } <*> Parser {runParser = runFn2 } = Parser { runParser = \s -> do
+            (outputFn, rest1) <- runFn1 s
+            (parserOutput, rest2) <- runFn2 rest1
+            return (outputFn parserOutput, rest2)
         }
 
 instance Monad Parser where
-    (Parser x) >>= fn = Parser { runParser = \inputStr -> do
-        (output, remainderStr) <- x inputStr
+    (Parser x) >>= fn = Parser { runParser = \s -> do
+        (output, rest) <- x s
         -- (fn output) is Parser B so let's run it with the remainder!
-        runParser (fn output) remainderStr  
+        runParser (fn output) rest  
     }
+
+class (Applicative f) => Alternative f where
+  empty     :: f a
+  (<|>)     :: f a -> f a -> f a 
+  some      :: f a -> f[a]
+  some v  = some_v
+    where many_v = some_v <|> pure []
+          some_v = (:) <$> v <*> many_v
+  many      :: f a -> f [a]
+  many v  = many_v
+    where many_v = some_v <|> pure []
+          some_v = (:) <$> v <*> many_v
+
+
+instance MonadFail Parser where
+    fail _ = Parser { runParser = \s -> Nothing }
+
+instance Alternative Parser where
+    empty = fail ""
+    (Parser x) <|> (Parser y) = Parser { runParser = \s -> 
+        case x s of
+            Just x -> Just x
+            Nothing -> y s
+    }
+
 
 
 main :: IO ()
 main = do
-
   print "Done."
